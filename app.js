44ac13235bc498245ef9448cc61eb61438ad7d05
Ainel <ainelainel@hotmail.com>
2020-09-20 20:11:17 +0200

    Add api and mock data

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+app.get('/api/downloads', (req, res) => {
+    fs.readFile(`${__dirname}/data/downloads.json`, 'utf-8', (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get('/api/stats', (req, res) => {
+    fs.readFile(`${__dirname}/data/stats.json`, 'utf-8', (err, data) => {
+        res.send(data)
+    })
+})
+
+const server = app.listen(3000, () => {
+    const port = server.address().port
+    console.log(`Server started on ${port}`)
+})
